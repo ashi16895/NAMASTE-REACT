d8c68d6e06620f3b3479e650336059b148873474
Ashi Bajpai <ashibajpai@Ashis-MacBook-Air.local>
2024-01-04 18:24:06 +0530

    basic code

--- App.js
+++ App.js
@@ -1,2 +1,37 @@
 import React from "react"; 
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+
+
+//React.createElement =>React Element - JS Object => HTMLElement(render)
+//JSX => JavaScript Syntax to create React element, HTML like syntax;
+//JSX (transpiled before it reaches to JS Engine) - PARCEL(manager) - Babel transpiler(JSX to React Code)
+//JSX =>babel transpiles it to React.createElement => React Element - JS Object => HTMLElement(render)
+
+
+
+const title =  (
+<h1>
+    
+    Namaste React using JSX
+</h1>
+)
+
+//React Component
+//Class based components - OLD
+//functional components - NEW
+//React Functional Component = javascript functions
+
+
+//Component Composition
+const HeadingComponent = () => (
+
+    <div id="container">
+        {title}
+        {/* <Title/> */}
+    <h2 className="heading">Namaste React Functional Components</h2>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
